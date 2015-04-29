8a4f0f891320b080873dcf0454d1749b7fdcc769
ChrisGin <christiandmartinez@student.hartnell.edu>
2015-04-29 14:32:37 -0800

    Camera functions

--- app/src/main/java/com/example/chrisgin/imagefilterpuzzlegame/MainActivity.java
+++ app/src/main/java/com/example/chrisgin/imagefilterpuzzlegame/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.chrisgin.imagefilterpuzzlegame;
 
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 
 public class MainActivity extends ActionBarActivity {
     private Button mGalleryButton;
+    private Button mCameraButton;
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    View view;
     public final static String EXTRA_MESSAGE = "com.example.chrisgin.imagefilterpuzzlegame.MESSAGE";
 
     @Override
@@ -18,17 +34,40 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       mGalleryButton = (Button) findViewById(R.id.galleryButton);
-       mGalleryButton.setOnClickListener(new View.OnClickListener(){
+        mGalleryButton = (Button) findViewById(R.id.galleryButton);
+        mCameraButton = (Button) findViewById(R.id.cameraButton);
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+        mGalleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 Intent intent = new Intent(MainActivity.this, Gallery.class);
                 startActivity(intent);
+
             }
         });
 
     }
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                ImageView imageView = (ImageView) view.findViewById(R.id.camera_image);
+                ;
+                imageView.setImageURI(null);
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -37,7 +76,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -50,7 +89,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void changeMethod(View v){
+    public void changeMethod(View v) {
         startActivity(new Intent(MainActivity.this, Gallery.class));
     }
 
